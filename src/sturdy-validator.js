b01d4a4b37b0f6b6b35479a1a4b37f7750840278
Matti Salokangas <matti.salokangas@gmail.com>
2015-02-14 16:40:06 -0800

    Use factory pattern to load validation factories

--- src/sturdy-validator.js
+++ src/sturdy-validator.js
@@ -4,15 +4,47 @@
 
   var pluginName = "validate",
     defaults = {
-    enabled: true,
-    success: function() {},
-    fail: function() {},
-    error: function() {}
-  };
+      enabled: true,
+      defaultEvent: 'keyup',
+      defaultType: 'email',
+      success: function() {},
+      fail: function() {},
+      error: function() {}
+    };
+
+  function Validator(type, validate) {
+    this.type = type;
+    this.validate = validate;
+  }
+
+  $.extend(Validator.prototype, {
+    type: null,
+    validate: function () {}
+  });
+
+  var EmailValidator = new Validator('email', function(value) {
+    return value.match(/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/);
+  });
+
+  function ValidatorFactory () {
+    this.factories = [
+      EmailValidator
+    ];
+  }
+
+  $.extend(ValidatorFactory.prototype, {
+    lookup: function (type) {
+      var index = this.factories.map(function(factory) {
+        return factory.type;
+      }).indexOf(type);
+      return this.factories[index];
+    }
+  });
 
   function Plugin (element, options) {
     this.element = element;
     this.settings = $.extend({}, defaults, options);
+    this._validatorFactory = new ValidatorFactory();
     this._defaults = defaults;
     this._name = pluginName;
     this.init();
@@ -30,19 +62,22 @@
     processElement: function(el, settings) {
       settings = $.extend({}, defaults, settings);
 
+      var self = this;
+      el.on(settings.event || settings.defaultEvent, function() {
+        self.validate(el, settings);
+      });
+
+      return el;
+    },
+    validate: function (el, settings) {
+      var factory = this._validatorFactory.lookup(settings.type || settings.defaultType);
       try {
-        (this.validate(el, settings) ?
+        (factory.validate(el.val()) ?
           settings.success :
           settings.fail)();
       } catch(e) {
         settings.error(e);
       }
-
-      return el;
-    },
-    validate: function (el, settings) {
-      // TODO: Implement this method.
-      return true;
     }
   });
 
